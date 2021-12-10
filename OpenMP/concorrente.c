031ac27dc7b087254f9d8074c7ed6d4fc8b7983f
rafaelmocorrea <rafaelmocorrea@gmail.com>
2021-12-10 14:54:13 -0300

    Alterado o OpenMP

--- OpenMP/concorrente.c
+++ OpenMP/concorrente.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <omp.h>
+#include <time.h>
 
 #define VIVO 1
 #define MORTO 0
 #define HIGH 1
 #define NORMAL 0
-#define NUM_THREADS 2
 
 void imprime_matriz(int** matriz, int n){
-    printf("\n");
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
             printf("%d ", matriz[i][j]);
@@ -85,27 +83,36 @@ void copia_matriz(int **destino, int **origem, int n){
     }
 }
 
-int main(){
+int main(int argc, char **argv){
 
     int **grid;             //grid atual
     int **newgrid;          //proximo grid
-    int N = 2048;             //NxN
-    int iteracoes = 2000;      //Numero de iteracoes
+    int N = 2048;           //NxN
+    int iteracoes = 2000;    //Numero de iteracoes
     int cont_aux;           //Contador auxiliar
-    int tipo = HIGH;        //Altera o tipo do jogo
-    int soma = 0;
-    int m,n;
+    int tipo = HIGH;        //Tipo do jogo
+    int i, j, k, l;         //Auxiliares dos lacos
+    int n_proc = 1;
     struct timespec inicio_prog, fim_prog;
     long total_diff;
 
+    if (argc != 2) {
+      printf("Uso: %s <num_processadores>\n", argv[0]);
+      return 0;
+    }
+
+    n_proc = atoi(argv[1]);
+
     clock_gettime(CLOCK_MONOTONIC_RAW, &inicio_prog);
+
+    printf("Numero de processadores escolhidos: %d\n",n_proc);
 
     grid = calloc(N, sizeof(int *));
     newgrid = calloc(N, sizeof(int *));
 
-    for (int i = 0; i < N; i++){
-        grid[i] = calloc(N,sizeof(int));
-        newgrid[i] = calloc(N,sizeof(int));
+    for (j = 0; j < N; j++){
+        grid[j] = calloc(N,sizeof(int));
+        newgrid[j] = calloc(N,sizeof(int));
     }
 
     glider(grid);
@@ -113,51 +120,49 @@ int main(){
 
     printf("Condicao inicial: %d",somaMatriz(grid,N));
 
-
-    for (int i = 0; i < iteracoes; i++){
-        for (int k = 0; k < N; k++){
-            for (int l = 0; l < N; l++){
-                cont_aux = getNeighbors(grid,k,l,N);
-                if (grid[k][l] == VIVO){
-                    if (cont_aux < 2 || cont_aux >= 4){
-                        newgrid[k][l] = MORTO;
-                    } else {
-                        newgrid[k][l] = VIVO;
-                    }
-                } else {
-                    if (tipo != HIGH){
-                        if (cont_aux == 3){
-                            newgrid[k][l] = VIVO;
-                        } else {
+    for (i = 0; i < iteracoes; i++){
+        #pragma omp parallel num_threads(n_proc)
+            {
+            #pragma omp for private(k,l,cont_aux) collapse(2)
+            for (k = 0; k < N; k++){
+                for (l = 0; l < N; l++){
+                    cont_aux = getNeighbors(grid,k,l,N);
+                    if (grid[k][l] == VIVO){
+                        if (cont_aux < 2 || cont_aux >= 4){
                             newgrid[k][l] = MORTO;
+                        } else {
+                            newgrid[k][l] = VIVO;
                         }
                     } else {
-                        if (cont_aux == 3 || cont_aux == 6){
-                            newgrid[k][l] = VIVO;
+                        if (tipo != HIGH){
+                            if (cont_aux == 3){
+                                newgrid[k][l] = VIVO;
+                            } else {
+                                newgrid[k][l] = MORTO;
+                            }
                         } else {
-                            newgrid[k][l] = MORTO;
+                            if (cont_aux == 3 || cont_aux == 6){
+                                newgrid[k][l] = VIVO;
+                            } else {
+                                newgrid[k][l] = MORTO;
+                            }
                         }
                     }
                 }
             }
         }
         copia_matriz(grid,newgrid,N);
-        if (i+1 == iteracoes){
-            #pragma omp parallel num_threads(NUM_THREADS) private(m,n) reduction(+:soma)
-            {
-                #pragma omp for
-                for (m = 0; m<N;m++){
-                    for (n=0;n<N;n++){
-                        soma += grid[m][n];
-                    }
-                }
-            }
-            printf("\nSoma iteracao %d: %d",i+1,soma);
-        }
+        if (i+1 == iteracoes)
+            printf("\nGeracao %d: %d",i+1,somaMatriz(grid,N));
     }
+
     clock_gettime(CLOCK_MONOTONIC_RAW, &fim_prog);
+
     total_diff = ((fim_prog.tv_sec - inicio_prog.tv_sec)*(1000*1000*1000) + (fim_prog.tv_nsec - inicio_prog.tv_nsec)) / 1000000;
-    printf("Tempo total: %lums\n", total_diff);
+
+    printf("\nTempo total: %lums\n", total_diff);
+
+
     free(grid);
     free(newgrid);
 
